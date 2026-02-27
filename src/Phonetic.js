2d400c348c18c44351c24ed3aa241e0ac430fed7
Shalini-jadala <jspforever143@gmail.com>
2026-02-27 14:37:37 +1100

    update phonetic.js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,8 @@ import React from "react";
 export default function Phenotic(props){
     return(
         <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank">
+            <a href={props.phonetic.audio} target="_blank"
+               rel="noopener noreferrer">
                 Listen
             </a>
             <br />
