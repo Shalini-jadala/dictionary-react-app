fd798acb54898874234edf6fdad4af3caa421c36
Shalini-jadala <jspforever143@gmail.com>
2026-03-02 15:41:20 +1100

    added photos section to dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,21 +2,33 @@ import React, { useState} from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos"
 
 export default function Dictionary(props){
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
     function handleResponse(response){
         setResults(response.data[0]);
     }
 
-    function search(){
+    function handlePexelsResponse(response){
+      setPhotos(response.data.photos);
+    }
+
+   function search(){
       let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
       axios.get(apiUrl).then(handleResponse);
-    }
-   
+    
+
+      let pexelsApiKey = "8yMXq7X8o3SxBMfgRd053aOM7apFa7kIpteR9oLpT1xaZYdFm9vx0XEy";
+      let pexelsApiUrl = 
+      `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+      let headers = { Authorization: `${pexelsApiKey}`};
+      axios.get(pexelsApiUrl, { headers: headers}).then(handlePexelsResponse);
+   }
    
     function handleSubmit(event){
     event.preventDefault();
@@ -46,6 +58,7 @@ export default function Dictionary(props){
            </form>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
         
     </div>
 
